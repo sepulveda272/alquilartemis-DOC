13a8bcf21a897e8fbb1dd2f445adfb0d3dfe1c05
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2023-06-23 20:06:42 -0500

    feat: :tada: avance de la la DOC

---
+++ backend/src/app.js
@@ -0,0 +1,9 @@
+//9. se importa el express
+import express from "express";
+//10. luego vamos al package.json y debajo de "description" ponemos "type": "module"
+
+//11. se crea una constante donde se guarda todo el poder del express
+const app = express();
+
+//12. se exporta la constante app
+export default app;
---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+//7. se hace el metode main
+const main = () => {
+
+}
+
+//.8 se invoca la constante main
+main();
+
+//contunuacion de en app.js
